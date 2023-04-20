3b7cac100de9baec451c2165d51353815b7c64b7
julianoni <52756828+julianoni@users.noreply.github.com>
2023-04-20 15:21:50 -0600

    Add files via upload

---
+++ src/App.vue
@@ -0,0 +1,26 @@
+<template>
+  <img alt="Vue logo" src="./assets/JLogo.png" height="800">
+  <HelloWorld msg="Howdy yall welcome to my portfolio page!"/>
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld.vue'
+
+export default {
+  name: 'App',
+  components: {
+    HelloWorld
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
